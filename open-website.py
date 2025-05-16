2d277d43cc3832d4e70e4e91682da9b6b2d6d927
F9. <vxqrera@gmail.com>
2025-05-16 19:27:56 -0400

    Create open-website.py

---
+++ open-website.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+
+__author__ = "F9"
+__version__ = "1.0.0"
+__github__ = "https://github.com/F9presents"
+
+
+
+
+print ("type the url you want to open")
+url = "https://github.com/F9presents" #type the url you want to open
+webbrowser.open(url)
+
+
+
