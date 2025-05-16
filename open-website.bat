6ce2bfe0b18082b55d9dc18af4ed574e7d93140e
F9. <vxqrera@gmail.com>
2025-05-16 19:28:17 -0400

    Update open-website.bat

--- open-website.bat
+++ open-website.bat
@@ -3,6 +3,6 @@ title Website Test by @F9
 
 
 
-echo Type the website you want to open (e.g. https://example.com)
+echo Type the website you want to open (e.g. https://github.com/F9presents)
 set /p url=Website: 
 start "" "%url%"
